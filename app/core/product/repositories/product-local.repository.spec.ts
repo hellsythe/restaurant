72f7fa5b20001ceff4d507a94547673f5b6706b6
Camilo Rodriguez <camilo@sdkconsultoria.com>
2025-01-07 00:18:42 +0000

    wip

--- app/core/product/repositories/product-local.repository.spec.ts
+++ app/core/product/repositories/product-local.repository.spec.ts
@@ -5,14 +5,22 @@ import { ProductLocalRepository } from './product-local.repository';
 import { UuidGenerator } from '@/app/core/global/services/uuid-generator';
 
 describe('ProductLocalRepository', () => {
-   test('create product', async () => {
-      const repository = container
-         .createChildContainer()
-         .register<UuidGenerator>("UuidGenerator", { useValue: { generate: () => '123' } })
-         .resolve(ProductLocalRepository);
+   const repository = container
+   .createChildContainer()
+   .register<UuidGenerator>("UuidGenerator", { useValue: { generate: () => '123' } })
+   .resolve(ProductLocalRepository);
 
+   test('create product', async () => {
       const id = await repository.create({ name: 'test', price: 100 });
 
       expect(id).toBe('123');
+   });
+
+   test('update product', async () => {
+      await repository.create({ name: 'test', price: 100 });
+      await repository.update('123', { name: 'anime', price: 100 });
+      const product = await repository.findById('123');
+
+      expect(product.name).toBe('anime');
    });
 });
