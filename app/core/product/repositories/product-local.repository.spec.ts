17d40a052cd97aaca004794db7126c158b8109eb
Camilo Rodriguez <hellsythex@gmail.com>
2025-01-07 09:20:23 -0600

    e

--- app/core/product/repositories/product-local.repository.spec.ts
+++ app/core/product/repositories/product-local.repository.spec.ts
@@ -1,16 +1,36 @@
 
-import { describe, test, expect } from '@jest/globals';
+import { describe, test, expect, jest } from '@jest/globals';
 import { container } from '../product.dependencies';
 import { ProductLocalRepository } from './product-local.repository';
 import { UuidGenerator } from '@/app/core/global/services/uuid-generator';
+import { before } from 'node:test';
+jest.mock('@/app/core/global/services/uuid-generator', () => {
+   return {
+      UuidGenerator: jest.fn().mockImplementation(() => {
+       return {generate: () => {'generate'}};
+     }),
+   };
+ });
+//  jest.mock('@/app/core/global/services/uuid-generator', () => ({ MyClass: jest.fn() }));
+
 
 describe('ProductLocalRepository', () => {
+   const uuidGenerator = jest
+   .fn(() => 'generate')
+   .mockImplementationOnce(() => '123')
+   .mockImplementationOnce(() => '456');
+
    const repository = container
    .createChildContainer()
-   .register<UuidGenerator>("UuidGenerator", { useValue: { generate: () => '123' } })
+   .register<UuidGenerator>("UuidGenerator", { useValue: { generate: uuidGenerator } })
    .resolve(ProductLocalRepository);
 
+   beforeEach(() => {
+      uuidGenerator.mockClear();
+   });
+
    test('create product', async () => {
+      // UuidGenerator.mock
       const id = await repository.create({ name: 'test', price: 100 });
 
       expect(id).toBe('123');
